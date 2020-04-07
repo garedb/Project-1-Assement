a16153b62e3ebfaa6dca439e8fc4dc5fa5336525
garedb <garedbarney@gmail.com>
2020-04-06 18:03:26 -0700

    updated JS files, now working

--- js/main.js
+++ js/main.js
@@ -1,18 +1,42 @@
-console.log('hey there');
-
 let Ans = document.getElementById('answer');
 let Num = document.getElementById('input');
 let Add = document.getElementById('plus');
 let Sub = document.getElementById('minus');
 
- 
+ 	let count = 0
+ 	
+ 	function adding () {
+ 		let amountToAdd = parseInt(Num.value)
+ 		count += amountToAdd
+ 		// Update display function
+ 		updateDis()
+ 	}
+ 	
+ 	function subtracting () {
+ 		let amountToSub = parseInt(Num.value)
+ 		count -= amountToSub
+ 		// Update display Function
+ 		updateDis()
+ 	}
+ 	
+ 	function updateDis() {
+ 		Ans.innerText = count
+ 		if (count < 0) {
+ 			Ans.style.color = 'red'
+ 		} else {
+ 			Ans.style.color = 'black'
+ 		}
+ 	}
+	
 	Add.addEventListener('click', () => {
-		console.log('clicked')
-		let Ans = (Num.value ++)
+		// console.log('clicked')
+		// add function
+		adding() 
 
 	});
 
 	Sub.addEventListener('click', () => {
-		console.log('clicked1')
-		let Ans = (Num.value --)
+		// console.log('clicked1')
+		// subtract function
+		subtracting()
 	});
